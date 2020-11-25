047e57a97518df504fe99c9d01ff6f5f40d63c27
Dhanush <dhanuraj7258@gmail.com>
2020-11-25 17:30:31 +0530

    Add footer and navbar active class

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,84 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
+import { animateScroll as scroll } from "react-scroll";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialLink,
+} from "./FooterElements";
+
+const toggleHome = () => {
+  scroll.scrollToTop();
+};
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Terms or Service</FooterLink>
+            </FooterLinkItems>
+
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/signin">Contact</FooterLink>
+              <FooterLink to="/signin">Support</FooterLink>
+              <FooterLink to="/signin">Sponsorships</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin">Github</FooterLink>
+              <FooterLink to="/signin">Instagram</FooterLink>
+              <FooterLink to="/signin">Facebook</FooterLink>
+              <FooterLink to="/signin">Linkedin</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="" onClick={toggleHome}>
+              Logo
+            </SocialLogo>
+            <WebsiteRights>
+              Logo &copy; {new Date().getFullYear()}
+              All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialLink href="" target="_blank">
+                <FaGithub />
+              </SocialLink>
+              <SocialLink href="" target="_blank">
+                <FaFacebook />
+              </SocialLink>
+              <SocialLink href="" target="_blank">
+                <FaInstagram />
+              </SocialLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
