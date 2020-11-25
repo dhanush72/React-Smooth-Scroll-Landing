7d1516177761a79c9766e0d7b74061d644e37388
Dhanush <dhanuraj7258@gmail.com>
2020-11-25 12:00:41 +0530

    Add navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,45 @@
 import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtnLink,
+  NavBtn,
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo>Logo</NavLogo>
+          <NavLogo to="/">
+            <h4>Logo</h4>
+          </NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtnLink to="/signin">
+            <NavBtn>Sign In</NavBtn>
+          </NavBtnLink>
         </NavbarContainer>
       </Nav>
     </>
